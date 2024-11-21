97b90d1c7aa9f2e5624a5aa5559f4e78cfdf71a5
1i-1ith <121597121+1i-1ith@users.noreply.github.com>
2024-11-21 16:40:50 +0300

    Create Sheet.cpp

---
+++ Sheet.cpp
@@ -0,0 +1,13 @@
+#include "Sheet.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args)
+{
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    CartesianSheet::Sheet form;
+    Application::Run(% form);
+}
